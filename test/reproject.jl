b37a7125d27a0b7b0cbb34170ee70cd073c75096
Fabian Gans <fgans@bgc-jena.mpg.de>
2025-10-07 09:41:33 +0200

    Update test/reproject.jl
    Co-authored-by: Anshul Singhvi <anshulsinghvi@gmail.com>

--- test/reproject.jl
+++ test/reproject.jl
@@ -51,7 +51,7 @@ for sourcetype in sourcetypes
         source = SST.ProjectionSource(sourcetype,a)
         target = SST.ProjectionTarget(targettype,targetargs...;targetkwargs...)
 
-        projar = SST.LazyProjectedDiskArray(source,target)
+        projar = @test_nowarn SST.LazyProjectedDiskArray(source,target)
 
         # We first compute the "true" results by reprojecting every point from target to source
         targetcoords_unitsphere = (SST.index_to_unitsphere.(LinearIndices(projar),(target.tree,)))
